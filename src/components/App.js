c358155b8dd4731572db42694f3c0342c9ea8dbc
Monali Gupta <monaligupta06@gmail.com>
2025-10-17 01:11:26 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,19 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+{/* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */}
 
+import React from 'react'
+import Reviews from './Reviews'
+
+const App = () => {
+  return (
+    <div>
+      <section>
+         <h1 id="review-heading">Our Reviews</h1>
+
+          <main>
+            <Reviews/>
+          </main>
+      </section></div>
+  )
+}
+
+export default App
